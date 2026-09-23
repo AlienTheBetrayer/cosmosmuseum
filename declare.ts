85b44756adb815150c3c9b02058924e7d1e8864d
alienthebetrayer <gpichkurov@gmail.com>
2026-09-23 16:05:16 +0300

    Update declare.ts

--- declare.ts
+++ declare.ts
@@ -1,14 +1,10 @@
 import { Temporal } from 'temporal-polyfill';
 
-// Vercel's Edge and Serverless environments use 'globalThis' 
+// Safely attach it to the environment runtime without redefining the TS variable
 if (!globalThis.Temporal) {
   Object.defineProperty(globalThis, 'Temporal', {
     value: Temporal,
-    writable: false, // Prevent Next.js HMR or Vercel bundling cycles from overwriting it
+    writable: false,
     configurable: true,
   });
-}
-
-declare global {
-  var Temporal: typeof import('temporal-polyfill').Temporal;
 }
