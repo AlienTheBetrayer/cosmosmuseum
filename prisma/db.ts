a1e8577091688a2edf21c7ecfd125f824179c0d2
alienthebetrayer <gpichkurov@gmail.com>
2026-09-23 15:48:14 +0300

    temporal fix

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,4 +1,6 @@
 import "server-only"
+import "temporal-polyfill/full/global";
+import "temporal-polyfill/types/global";
 
 import postgres from "@prisma/orm-postgres/runtime";
 import type { Contract, FieldOutputTypes } from "./contract.d";
